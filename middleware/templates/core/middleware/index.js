7e07ce74085bcbddbec6e896259b0d179278c6fe
Robert Haritonov <r@rhr.me>
2015-03-15 20:07:08 +0100

    Improving middleware starting template

--- middleware/templates/core/middleware/index.js
+++ middleware/templates/core/middleware/index.js
@@ -1,12 +1,41 @@
-// NodeJS module, included automatically from app.js
-// ExpressJS middleware http://expressjs.com
+/*
+ * NodeJS module, included automatically from SourceJS app.js
+ * ExpressJS middleware http://expressjs.com
+ * */
 
-var processRequest = function (req, res) {
-    // Process res data
+var deepExtend = require('deep-extend');
+
+// Module configuration
+var globalConfig = global.opts.plugins && global.opts.plugins.pluginName ? global.opts.plugins.pluginName : {};
+var config = {
+    enabled: true,
+
+    // Public object is exposed to Front-end via options API.
+    public: {}
 };
 
-exports.process = function (req, res, next) {
-    processRequest(req, res);
+// Overwriting base options
+deepExtend(config, globalConfig);
 
-    next();
+/*
+ * @param {object} req - Request object
+ * @param {object} res - Response object
+ * @param {function} next - The callback function
+ * */
+var processRequest = function (req, res, next) {
+    if (!config.enabled) {
+        next();
+        return;
+    }
+
+    // Check if request is targeting Spec
+    if (req.specData && req.specData.renderedHtml) {
+        // Process req data
+
+        next();
+    } else {
+        next();
+    }
 };
+
+exports.process = processRequest;
